baa13381b25ac54beb4ed6c43bd8aad13b321108
Fernanda Fernandes Lima <83232996+feh-projetos@users.noreply.github.com>
2021-08-27 18:05:18 -0300

    Update Exercio 3 .js

--- Exercio 3 .js
+++ Exercio 3 .js
@@ -1,7 +1,7 @@
  for (let i=0;i<11;i++) {
     document.write("<p> Tabuada do "+i+"</p">) ;
    for (var j = i; j <11; j++ ){
-     document.write(i + "x" + j+ " " + (i*J)+"br" );
+     document.write(i + "x" + j+ " = " + (i*J)+"br" );
         
       
     }
